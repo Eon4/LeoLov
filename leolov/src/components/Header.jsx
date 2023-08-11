1b1748423b2cbba086aea41203676bf5356d8d70
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-11 12:11:19 +0200

    update

---
+++ leolov/src/components/Header.jsx
@@ -0,0 +1,14 @@
+// import style from '../styles/header.module.scss'
+
+import Headerbg from '../assets/images/header.jpg'
+
+export function Header(){
+    return (
+        <header>
+            <h1>Leo-Lov</h1>
+            <p>Lov og ret kan være et plaster! Hvis du ellers har knaster!</p>
+        </header>
+
+    )
+}
+
--- leolov/src/pages/MainPage.jsx
+++ leolov/src/pages/MainPage.jsx
@@ -1,10 +1,21 @@
 import { Navigation } from "../components/navigation";
+import { Header } from '../components/Header';
+// import { Cards } from './component/cards/Cards';
+// import { Content } from './component/SectionOne';
+// import { Content } from './component/SectionTwo';
+// import { Footer } from './component/Footer';
 
 export const MainPage = () => {
     return (
       <>
         <Navigation />
- 
+        <Header/>
+        {/* <Cards/>
+      <SectionOne/>
+      <SectionTwo/>
+      <Footer/> */}
       </>
     );
   };
+
+  
